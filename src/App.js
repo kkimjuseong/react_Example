64afa8f9984e0629342e1637cac6510537a71652
kkimjuseong <rofodzl1@naver.com>
2024-06-15 00:06:03 +0900

    # app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
 import './App.css';
 
-function App() {
+// 기본 더미 데이터
+const DUMMY_DATA = [
+  {
+    id: 'g1',
+    text: '리액트 컴포넌트 스타일링 마스터하기'
+  },
+  {
+    id: 'g2',
+    text: 'UI/UX 프로그래밍 쌉고수되기'
+  },
+];
+
+const App = () => {
+
+  const [goals, setGoals] = useState(DUMMY_DATA);
+
+  // 새로운 목표를 추가하는 함수
+  const addGoalHandler = (userInput) => {
+    setGoals((prevGoals) => [
+      ...prevGoals,
+      { id: `g${prevGoals.length + 1}`, text: userInput },
+    ]);
+  };
+
+  // 목표를 삭제하는 함수
+  const deleteGoalHandler = (goalsId) => {
+    setGoals((prevGoals) => {
+      return prevGoals.filter(goal => goal.id !== goalsId);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <section id="goal-form">
+          <CourseInput onAddGoal={addGoalHandler}/>
+        </section>
+        <section id="goals">
+          <CourseList items={goals} onDeleteGoal={deleteGoalHandler}/>
+        </section>
+      </div>
   );
-}
+};
 
 export default App;
